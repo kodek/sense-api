e05c5f621e0857e74c2bdb062023ce15a1965cd7
Hernan Cosi <hernan@hernancosi.com>
2018-08-25 16:36:03 -0700

    Add realtime example

---
+++ cmd/example/example.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/kodek/sense-api"
+)
+
+func main() {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+	client, err := sense_api.NewClient(os.Getenv("SENSE_USER"), os.Getenv("SENSE_PASSWORD"))
+	if err != nil {
+		panic(err)
+	}
+	recv, done, err := client.Realtime()
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		throttle := time.Tick(1 * time.Second)
+		for val := range recv {
+			<-throttle
+			fmt.Printf("Consumption [%.2f] Production [%.2f]\n", val.Payload.W, val.Payload.SolarW)
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
+	done <- struct{}{}
+	time.Sleep(2 * time.Second)
+}
